6f0925a5181e449854ccb62e79eeed866bb901d5
Kara6000 <aqwkarahann@gmail.com>
2021-02-15 21:14:36 +0300

    added name to homepage

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -10,6 +10,17 @@ function Homepage() {
       <div className="search">
         <Search/>
       </div>
+      <div className="homepage-text">
+        <div className="homepage-text-header">
+          <span className="homepage-text-header-firstletter">C</span>HALLENGER <span className="homepage-text-header-firstletter">B</span>UILDS
+        </div>
+        <div className="homepage-text-content">
+          THIS WEBSITE IS UNDER CONSTRUCTION.
+        </div>
+        <div className="homepage-text-content">
+          DATA HAS BEEN FETCHED ONLY FOR THE CHAMPION "OLAF".
+        </div>
+      </div>
     </div>
     
   );
