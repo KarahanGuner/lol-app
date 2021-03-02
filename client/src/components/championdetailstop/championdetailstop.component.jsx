2e9bba52d8bb64b5917e1aab5cc5829375575513
Kara6000 <aqwkarahann@gmail.com>
2021-03-02 13:15:05 +0300

    fixed the issue of champion names looking like TwistedFate instead of Twisted Fate on championdetailstop

--- client/src/components/championdetailstop/championdetailstop.component.jsx
+++ client/src/components/championdetailstop/championdetailstop.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './championdetailstop.styles.css';
-import {currentPatch} from '../../miscData.js';
+import {currentPatch, championNames} from '../../miscData.js';
 
 function ChampionDetailsTop(props) {
   const {championDdragonName, numberOfGames} = props;
@@ -8,7 +8,7 @@ function ChampionDetailsTop(props) {
     <div className="championdetailstop">
       <img className="champion-picture" src={`https://ddragon.leagueoflegends.com/cdn/${currentPatch}/img/champion/${championDdragonName}.png`} alt="champion"/>
       <div className="championdetailstop-textgroup">
-        <div className="championdetailstop-championname">{championDdragonName} </div>
+        <div className="championdetailstop-championname">{championNames.find(champion => (champion.replace(/\s/g, '').toLowerCase()) == championDdragonName.toLowerCase())} </div>
         <div>Games Played:&nbsp;<span className="championdetailstop-thenumber">{numberOfGames}</span></div>
       </div>
 
