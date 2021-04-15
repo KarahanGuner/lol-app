f28ccaec239d5067c82bb4b8171be968d4827f03
Kara6000 <aqwkarahann@gmail.com>
2021-04-16 01:17:19 +0300

    added the new champion gwen, improved the responsive design for different screen sizes

--- client/src/components/championdetailsbottom/championdetailsbottom.component.jsx
+++ client/src/components/championdetailsbottom/championdetailsbottom.component.jsx
@@ -10,16 +10,16 @@ function ChampionDetailsBottom(props) {
   const [pageNumber, setPageNumber] = useState(1);
   const [matches, setMatches] = useState([]);
 
-  const observer = useRef();
+  const observer = useRef(); //returns an object with current property is what we initialize it with. this object lasts between re-renders but doesnt cause a re-render when it changes. that's its difference between "state".
   
-  const lastMatchElementRef = useCallback(node => {
-    if (observer.current) observer.current.disconnect();
-    observer.current = new IntersectionObserver(entries => {
+  const lastMatchElementRef = useCallback(node => { // whenever a new element is created with "lastMatchElementRef" the function inside of "useCallback" is run. this is a special interaction between "ref" attribute in elements and "useCallback"
+    if (observer.current) observer.current.disconnect(); //get rid of the previous IntersectionObserver
+    observer.current = new IntersectionObserver(entries => { //code inside "IntersectionObserver" is run whenever one of the entries is on the screen.
       if(entries[0].isIntersecting){
         setPageNumber(pageNumber + 1);
       }
     });
-    if (node) observer.current.observe(node);
+    if (node) observer.current.observe(node); //here we are telling "IntersectionObserver" to watch this node. "IntersectionObserver" takes its paramter "entries" from this "observe" method.
   });
 
   useEffect(() => {
