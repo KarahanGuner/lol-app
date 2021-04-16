8217be6ffb69b51a7d93ec30e175a7aa1095e320
Kara6000 <aqwkarahann@gmail.com>
2021-04-16 14:17:44 +0300

    made match details page more mobile responsive

--- client/src/pages/matchdetails/matchdetails.component.jsx
+++ client/src/pages/matchdetails/matchdetails.component.jsx
@@ -14,7 +14,6 @@ function MatchDetails() {
   const [ourParticipantId, changeOurParticipantId] = useState();
   champion = (champion.replace(/\s/g, '')).toLowerCase();//i want people to be able to enter the champion name into the address bar and immediately get the champion they want. i want both "master yi" and "masteryi" to work. in the miscData file there are two objects. we take the value from useParams() and change it to a lowercase no space in between version of itself and find the champions key id from "championNameKeyPairs" object. we use "championLowerCaseNameDdragonNamePairs" object to find how a champion is named in the champion.json file. we need to do this to access the information in the ddragon files.
   const championKey = championNameKeyPairs[champion];
-  const championDdragonName = championLowerCaseNameDdragonNamePairs[champion];//need this version of the champ name to read data from champion.json file
   useEffect(() => {
     axios({
       url: `/matchapi/${server}/${gameid}/${championKey}`,
@@ -88,60 +87,64 @@ function MatchDetails() {
           <div className="matchdetails-scoreboard-gameduration">Game Duration: {`${Math.floor(matchDetails[0].gameDuration/60)} Minutes`}</div>
         </div>
         <div className="matchdetails-timeline-container">
-          <div className="matchdetails-timeline-header">Timeline</div>
-          <div className="matchdetails-timeline-items">
-            {matchDetails[2].map((itemsInAMinute, i) => {
-              return <div className="matchdetails-timeline-itemsinaminute-witharrow" key={`timelineitemsInAMinute${i}`}><div  className="matchdetails-timeline-itemsinaminute"><div className="matchdetails-timeline-itemsinaminute-item-container">
-              {itemsInAMinute.map((item, j) => {
-              return <div key={`timelineitem${i}${j}`} ><img src={`https://ddragon.leagueoflegends.com/cdn/${currentPatch}/img/item/${item.itemId}.png`} alt="item"/>{item.type === "ITEM_SOLD" ? <i className="matchdetails-timeline-cross"></i> :null}</div>
-              })}
-              </div><div className="matchdetails-timeline-itemsinaminute-minute">{`${i} min`}</div></div>{i == matchDetails[2].length-1 ? null :<i className="matchdetails-arrow-right"></i>}</div>})
-            }
+          <div>
+            <div className="matchdetails-timeline-header">Timeline</div>
+            <div className="matchdetails-timeline-items">
+              {matchDetails[2].map((itemsInAMinute, i) => {
+                return <div className="matchdetails-timeline-itemsinaminute-witharrow" key={`timelineitemsInAMinute${i}`}><div  className="matchdetails-timeline-itemsinaminute"><div className="matchdetails-timeline-itemsinaminute-item-container">
+                {itemsInAMinute.map((item, j) => {
+                return <div key={`timelineitem${i}${j}`} ><img src={`https://ddragon.leagueoflegends.com/cdn/${currentPatch}/img/item/${item.itemId}.png`} alt="item"/>{item.type === "ITEM_SOLD" ? <i className="matchdetails-timeline-cross"></i> :null}</div>
+                })}
+                </div><div className="matchdetails-timeline-itemsinaminute-minute">{`${i} min`}</div></div>{i == matchDetails[2].length-1 ? null :<i className="matchdetails-arrow-right"></i>}</div>})
+              }
+            </div>
           </div>
         </div>
         <div className="matchdetails-abilityorder-container">
-          <div className="matchdetails-abilityorder-header">Ability Order</div>
-          <div className="matchdetails-abilityorder-content">
-            <table className="matchdetails-abilityorder-table">
-              <tbody>
-              <tr>
-                <th>Q</th>
-                {matchDetails[1].skillOrder.map((levelUp, i) => {
-                  if(levelUp.skillSlot == 1) {
-                    return <td key={`Q${i}`} className="matchdetails-abilityorder-table-chosendata">{`${i+1}`}</td>
-                  } 
-                  return <td key={`Q${i}E`}></td>
-                })}
-              </tr>
-              <tr>
-                <th>W</th>
-                {matchDetails[1].skillOrder.map((levelUp, i) => {
-                  if(levelUp.skillSlot == 2) {
-                    return <td key={`W${i}`} className="matchdetails-abilityorder-table-chosendata">{`${i+1}`}</td>
-                  } 
-                  return <td key={`W${i}E`}></td>
-                })}
-              </tr>
-              <tr>
-                <th>E</th>
-                {matchDetails[1].skillOrder.map((levelUp, i) => {
-                  if(levelUp.skillSlot == 3) {
-                    return <td key={`E${i}`} className="matchdetails-abilityorder-table-chosendata">{`${i+1}`}</td>
-                  } 
-                  return <td key={`E${i}E`}></td>
-                })}
-              </tr>
-              <tr>
-                <th>R</th>
-                {matchDetails[1].skillOrder.map((levelUp, i) => {
-                  if(levelUp.skillSlot == 4) {
-                    return <td key={`R${i}`} className="matchdetails-abilityorder-table-chosendata">{`${i+1}`}</td>
-                  } 
-                  return <td key={`R${i}E`}></td>
-                })}
-              </tr>
-              </tbody>
-            </table>
+          <div>
+            <div className="matchdetails-abilityorder-header">Ability Order</div>
+            <div className="matchdetails-abilityorder-content">
+              <table className="matchdetails-abilityorder-table">
+                <tbody>
+                <tr>
+                  <th>Q</th>
+                  {matchDetails[1].skillOrder.map((levelUp, i) => {
+                    if(levelUp.skillSlot == 1) {
+                      return <td key={`Q${i}`} className="matchdetails-abilityorder-table-chosendata">{`${i+1}`}</td>
+                    } 
+                    return <td key={`Q${i}E`}></td>
+                  })}
+                </tr>
+                <tr>
+                  <th>W</th>
+                  {matchDetails[1].skillOrder.map((levelUp, i) => {
+                    if(levelUp.skillSlot == 2) {
+                      return <td key={`W${i}`} className="matchdetails-abilityorder-table-chosendata">{`${i+1}`}</td>
+                    } 
+                    return <td key={`W${i}E`}></td>
+                  })}
+                </tr>
+                <tr>
+                  <th>E</th>
+                  {matchDetails[1].skillOrder.map((levelUp, i) => {
+                    if(levelUp.skillSlot == 3) {
+                      return <td key={`E${i}`} className="matchdetails-abilityorder-table-chosendata">{`${i+1}`}</td>
+                    } 
+                    return <td key={`E${i}E`}></td>
+                  })}
+                </tr>
+                <tr>
+                  <th>R</th>
+                  {matchDetails[1].skillOrder.map((levelUp, i) => {
+                    if(levelUp.skillSlot == 4) {
+                      return <td key={`R${i}`} className="matchdetails-abilityorder-table-chosendata">{`${i+1}`}</td>
+                    } 
+                    return <td key={`R${i}E`}></td>
+                  })}
+                </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
         <div className="matchdetails-runes-container">
